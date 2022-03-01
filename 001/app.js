d93a5d46b856bb85c8f6b741e4eb9802616e8534
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-01 12:20:07 +0200

    u2

--- 001/app.js
+++ 001/app.js
@@ -9,8 +9,20 @@ console.log('hello');
 const arr1 = [];
 
 for (let i = 0; i < 10; i++) {
-    const randomDigit = rand(1, 6);
+    const randomDigit = rand(1, 6); // tik del aiskumo
     arr1.push(randomDigit);
+    // arr1[i] = randomDigit; // imanoma, bet ne universalu!
 }
 
 console.log(arr1);
+
+
+let sum2 = 0;
+
+for (let i = 0; i < 22; i++) {
+    const randomDigit = rand(1, 6); // tik del aiskumo
+    sum2 = sum2 + randomDigit;
+    // sum2 += randomDigit;
+}
+
+console.log(sum2);
