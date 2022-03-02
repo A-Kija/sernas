5820e578d69e256ed4384aede73ea1e7a65c6eb9
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-02 12:03:25 +0200

    list

--- 002/app.js
+++ 002/app.js
@@ -16,3 +16,29 @@ rabbit.style.color = 'yellow';
 rabbit.style.backgroundColor = 'blue';
 
 console.log(rabbit);
+
+const bars = document.querySelector('h2 + h2');
+bars.style.color = 'brown';
+
+
+const li = document.querySelectorAll('li');
+const ul = document.querySelector('ul');
+
+console.log(li);
+
+// for (let i = 0; i < li.length; i++) {
+//     li[i].style.color = 'pink';
+// }
+
+// li.forEach(element => console.log(element.innerText));
+
+li.forEach((element) => {
+    element.style.color = 'red';
+    console.log(element.innerText);
+});
+
+console.log(ul.children);
+
+// ul.children.forEach(a => a);
+
+// li.style.color = 'brown';
