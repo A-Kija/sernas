59b81958574f1187a9a55c6a26489afffe351b8d
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-08 10:45:34 +0200

    obj

---
+++ 005/app.js
@@ -0,0 +1,29 @@
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const namas1 = {}; //objektas
+namas1.kaminai = 2;
+namas1.gyventojai = ['Vytas', 'Lina', 'Bebras'];
+namas1.adresas = 'Guobų g. 13';
+
+const namas2 = {
+    kaminai: 5,
+    gyventojai: ['Simas', 'Rimas'],
+    adresas: 'Guobų g. 13A'
+}
+
+
+console.log(namas1.kaminai);
+
+// namas1.gyventojai.forEach((g) => {
+//     console.log(g);
+// });
+
+for (let i = 0; i < namas1.gyventojai.length; i++) {
+    console.log(namas1.gyventojai[i]);
+}
+
+namas1.gyventojai.forEach(g => console.log(g));
