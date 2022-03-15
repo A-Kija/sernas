1fa8930a966222f67ac3ed72f4e38df1460c9bc5
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-15 12:17:10 +0200

    animal

--- 009/app.js
+++ 009/app.js
@@ -4,6 +4,8 @@ class Animal {
         this.type = t;
         this.sleep = false;
         this.food = 0;
+        this.distance = 0;
+        this.foodProcessing = this.rand(35, 75); // %
     }
 
     goSleep() {
@@ -16,8 +18,26 @@ class Animal {
 
     eat(amount) {
         if (!this.sleep) {
-            this.food += amount;
+            this.food += amount / 100 * this.foodProcessing;
         }
+    }
+
+    run(dist) {
+        if (!this.sleep) {
+            if (this.food >= dist) {
+                this.food -= dist;
+                this.distance += dist;
+            } else {
+                this.distance += this.food;
+                this.food = 0;
+            }
+        }
+    }
+
+    rand(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
 }
@@ -27,13 +47,17 @@ const lape = new Animal('Lapė');
 
 const meska = new Animal('Meška');
 
-meska.goSleep();
+// meska.goSleep();
 
-lape.eat(22.2);
+lape.eat(10);
+lape.eat(10);
+lape.eat(10);
 
-meska.eat(147.27);
+// meska.eat(10);
 
-lape.eat(8.8);
+lape.run(100);
+
+// lape.run(2.7487);
 
 // for (const what in meska) {
 //     console.log(what);
