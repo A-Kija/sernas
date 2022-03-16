268ac3d7d02b3ff604d0ef2139dc9d14d4ebd2dc
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-16 10:55:39 +0200

    anukas

--- 010/app.js
+++ 010/app.js
@@ -18,7 +18,6 @@ class K {
 }
 
 
-
 const obj1 = new K(5);
 const obj2 = new K(7);
 
@@ -26,8 +25,46 @@ obj2.changeAmount(77);
 obj2.changeCount(88);
 obj1.changeCount(100);
 
+// console.log('obj1', obj1);
+// console.log('obj2', obj2);
 
-console.log('obj1', obj1);
-console.log('obj2', obj2);
+// console.log('static:', K.count);
 
-console.log('static:', K.count);
+
+class Anukas {
+    static senelioPalikimas = 10000; // Eur
+
+    constructor() {
+        this.pinigai = this.rand(1000, 2000);
+    }
+
+    isleisti() {
+        if (this.pinigai >= 1000) {
+            this.pinigai = this.pinigai - 1000;
+            console.log('nusipirkau :)');
+        } else {
+            console.log('PINIGŲ NĖRA');
+        }
+    }
+
+    rand(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+}
+
+const simas = new Anukas();
+const rimas = new Anukas();
+const bimas = new Anukas();
+
+simas.isleisti();
+
+simas.isleisti();
+
+rimas.isleisti();
+
+console.log(simas);
+console.log(rimas);
+console.log(bimas);
+console.log('Liko: ', Anukas.senelioPalikimas);
