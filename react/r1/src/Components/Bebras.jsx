8f4345e328a39e58607a3166ed676a013651a2cf
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-22 12:20:48 +0200

    reactas

---
+++ react/r1/src/Components/Bebras.jsx
@@ -0,0 +1,13 @@
+function Bebras() {
+    return (
+        <div>
+            <h3>BEBRAS</h3>
+            <p>assets by status 6.91 KiB [cached] 1 asset
+assets by chunk 1.48 MiB (name: main)
+  asset static/js/bundle.js 1.48 MiB [emitted] (name: main) 1 related asset
+  asset main.30a0f0a5105ca99c4dcf.hot-update.js 3.76 KiB [emitted] [immutable] [hmr] (name: main) 1 related asset</p>
+        </div>
+    )
+}
+
+export default Bebras;
---
+++ react/r1/src/Components/Vovere.jsx
@@ -0,0 +1,21 @@
+function Vovere() {
+    return (
+        <div>
+            <h2 className="vov">VOVERE</h2>
+            <div>
+                Čia yra voverės 
+                <span>
+                    <div>
+                        Čia yra voverės 
+                        <span style={{fontSize:'60px'}}>
+                            komponentas
+                        </span>
+                    </div>
+                    komponentas
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Vovere;
