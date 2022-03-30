f933fa2714df5a6c5fa2e8628d652f212e890929
A-Kija <arvydas.kijakauskas@gmail.com>
2022-03-30 12:30:44 +0300

    KV

--- react/r1/src/App.jsx
+++ react/r1/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import MelynasisKvadratelis from './Components/MelynasisKvadratelis';
 import Pasaka from './Components/Pasaka';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
         <h1>HOOK</h1>
         <Pasaka></Pasaka>
+        <MelynasisKvadratelis></MelynasisKvadratelis>
     </div>
   );
 }
