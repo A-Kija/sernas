80190956e3d9415584295694c8df7dca88688251
A-Kija <arvydas.kijakauskas@gmail.com>
2022-04-19 12:26:02 +0300

    params

--- react/r1/src/App.jsx
+++ react/r1/src/App.jsx
@@ -3,6 +3,15 @@ import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
 import Office1 from './Routes/Office1';
 import Alabama from './Routes/Alabama';
+import Edit from './Routes/Edit';
+
+export const data = [
+    {n: 'barsukas', id: 5},
+    {n: 'bebras', id: 9},
+    {n: 'krokodilas', id: 15},
+    {n: 'vilkas', id: 775},
+    {n: 'zuikis', id: 29}
+];
 
 function App() {
   return (
@@ -12,12 +21,21 @@ function App() {
       <h1>ROUTER</h1>
         <Link to="/back-office" className="super">Back Office</Link>
         <Link to="/sweet-home" className="super">Alabama</Link>
+        <div>-----------------------------------------------</div>
+        {
+            data.map(a =><Link key={a.id} to={'/edit/' + a.id + '/' + a.n}>{a.n}</Link>)
+        }
+
+
+
+
 
 
         <Routes>
             <Route path="/" element={<div>Hello!</div>}></Route>
             <Route path="/back-office" element={<Office1></Office1>}></Route>
             <Route path="/sweet-home" element={<Alabama></Alabama>}></Route>
+            <Route path="/edit/:id/:name" element={<Edit></Edit>}></Route>
             <Route path="*" element={<div>404 not found</div>}></Route>
         </Routes>
         
