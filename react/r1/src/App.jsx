407ef07963d74245143e198a3537979b865cbd71
A-Kija <arvydas.kijakauskas@gmail.com>
2022-04-19 11:45:45 +0300

    routes

--- react/r1/src/App.jsx
+++ react/r1/src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css';
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+
+import Office1 from './Routes/Office1';
+import Alabama from './Routes/Alabama';
 
 function App() {
   return (
+
     <BrowserRouter>
     <div className="App">
       <h1>ROUTER</h1>
         <Link to="/back-office" className="super">Back Office</Link>
         <Link to="/sweet-home" className="super">Alabama</Link>
+
+
+        <Routes>
+            <Route path="/" element={<div>Hello!</div>}></Route>
+            <Route path="/back-office" element={<Office1></Office1>}></Route>
+            <Route path="/sweet-home" element={<Alabama></Alabama>}></Route>
+            <Route path="*" element={<div>404 not found</div>}></Route>
+        </Routes>
+        
     </div>
+    
     </BrowserRouter>
+
   );
 }
 
---
+++ react/r1/src/Routes/Office1.jsx
@@ -0,0 +1,13 @@
+function Office1() {
+
+
+    return (
+        <>
+            <h2 style={{
+                color: 'crimson'
+            }}>Welcome to Office</h2>
+        </>
+    )
+}
+
+export default Office1;
