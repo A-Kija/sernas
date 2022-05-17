e9b8136b11cb73f62d824c805442c56c5a0e551a
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-17 16:41:59 +0300

    buttons

--- react/r2/src/Components/Create.jsx
+++ react/r2/src/Components/Create.jsx
@@ -14,11 +14,25 @@ function Create() {
                 <div className="container p-0">
                     <div className="row">
                         <div className="col-4">
-                        <div className="form-group">
-                    <label>Tree height</label>
-                    <input type="text" className="form-control" />
-                    <small className="form-text text-muted">Tree height.</small>
-                </div>
+                            <div className="form-group">
+                                <label>Tree height</label>
+                                <input type="text" className="form-control" />
+                                <small className="form-text text-muted">Tree height.</small>
+                            </div>
+                        </div>
+                        <div className="col-8">
+                            <div class="form-group">
+                                <label>Tree type</label>
+                                <select class="form-control">
+                                    <option value="1">Leaf</option>
+                                    <option value="2">Spike</option>
+                                    <option value="3">Palm</option>
+                                </select>
+                                <small className="form-text text-muted">Tree type.</small>
+                            </div>
+                        </div>
+                        <div class="buttons">
+                        <button type="button" class="btn btn-outline-primary m-3">Add</button>
                         </div>
                     </div>
                 </div>
