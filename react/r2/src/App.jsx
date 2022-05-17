7c46d0dca19447112df26d11c9fbf39246612bc8
audronemotiejunaite <89936835+audronemotiejunaite@users.noreply.github.com>
2022-05-17 15:49:33 +0300

    bootstrap

--- react/r2/src/App.jsx
+++ react/r2/src/App.jsx
@@ -1,28 +1,60 @@
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import './bootstrap.css';
 import './App.css';
+
 
 function App() {
 
   const [trees, setTrees] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     axios.get('http://localhost:3003/trees-manager')
-    .then(res => {
-      console.log(res.data);
-      setTrees(res.data);
-    })
+      .then(res => {
+        console.log(res.data);
+        setTrees(res.data);
+      })
   }, []);
-  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Labas</h1>
-        {
-          trees.filter(t => t.name !== 'Agrastas').map(t => <div key={t.id}>{t.name}</div>)
-        }
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          One of three columns
+        </div>
+        <div className="col-8">
+
+          <div className="card m-4">
+            <div className="card-header">
+              Tree List
+            </div>
+            <div className="card-body">
+
+              <ul className="list-group">
+
+                {
+                  trees.filter(t => t.name !== 'Agrastas').map(t => <li className="list-group-item" key={t.id}>{t.name}</li>)
+                }
+              </ul>
+
+
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+      </div>
     </div>
   );
 }
